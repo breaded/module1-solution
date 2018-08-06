5757274d20761fc9b2584c44ee1ef07dfa7e40e7
breaded <blexing.ehigie@yahoo.com>
2018-08-05 22:32:47 -0300

    Assignment Module 1

---
+++ app.js
@@ -0,0 +1,35 @@
+(function (){
+'use strict';
+
+    angular.module('LunchCheck', [])
+
+        .controller('LunchCheckController', LunchCheckController);
+
+        LunchCheckController.$inject = ['$scope'];
+
+        function LunchCheckController($scope){
+
+            $scope.input = " ";
+            $scope.message =" ";
+            $scope.stack = function (){
+                var array = $scope.input.split(',');
+
+                if (array.length != ''  && array.length <= 3 ) {
+                    $scope.message = "Enjoy";
+                } else if (array.length > 3){
+                    $scope.message = "Too much";
+                } else {
+                    $scope.message = "Enter Data First";
+                }
+            
+            } ;
+
+        }
+
+
+
+
+
+
+
+})();
